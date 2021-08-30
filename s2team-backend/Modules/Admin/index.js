0b46c9127a0245ac490ae7b10b66c1491ca986b9
InnovativeWhales <innovativewhales@gmail.com>
2021-08-30 15:52:41 +0530

    updated SubAdmins Modules

--- s2team-backend/Modules/Admin/index.js
+++ s2team-backend/Modules/Admin/index.js
@@ -15,29 +15,38 @@ router.post(Constants.Routes.AddSubAdmin.Route, async function (req, res) {
             Constants.ressend({Message:"Message",Data:{},StatusCode:200,res:res,Status:"Sucess"})
     }
     catch(e){
-        if(e.constraint == "users_username_idx")
-            Constants.ressend({Message:"A user is already present with this username",Data:{},StatusCode:200,res:res,Status:"Error"})
+        console.log(e);
+        if(e.constraint == "users_username_key")
+            Constants.ressend({Message:"A user is already present with this username",Data:{},StatusCode:500,res:res,Status:"Error"})
         else
             Constants.ressend({Message:"Internal Error",Data:{},StatusCode:500,res:res,Status:"Error"})
     }
 })
 
 //Delete
-router.post(Constants.Routes.DeleteSubAdmin.Route, function (req, res) {
+router.post(Constants.Routes.DeleteSubAdmin.Route, async function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
-    //Add the record and end the response
-    Constants.ressend({Message:"Sucessfully Deleted",Data:req.decoded,StatusCode:200,res:res,Status:"Sucess"});
+    //Delete the record and end the response
+    let Data = await Db.query("Delete FROM users WHERE username = $1 And role = 'SubAdmin'",[username])
+    if(Data.rowCount === 1)
+        Constants.ressend({Message:"Sucessfully Deleted",Data:req.decoded,StatusCode:200,res:res,Status:"Sucess"});
+    else
+        Constants.ressend({Message:"User Not Found",Data:req.decoded,StatusCode:500,res:res,Status:"Error"});
 })
 
 //Update
-router.post(Constants.Routes.UpdateSubAdmin.Route, function (req, res) {
+router.post(Constants.Routes.UpdateSubAdmin.Route, async function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
-    //Add the record and end the response
-    Constants.ressend({Message:"Sucessfully Updated",Data:req.decoded,StatusCode:200,res:res,Status:"Sucess"});
+    //Update subadmin
+    let Data = await Db.query("Update users SET password = $1 WHERE username = $2 And role = 'SubAdmin' ",[await bcrypt.hash(password,Conts.SaltRounds),username])
+    if(Data.rowCount === 1)
+        Constants.ressend({Message:"Sucessfully Updated",Data:req.decoded,StatusCode:200,res:res,Status:"Sucess"});
+    else
+        Constants.ressend({Message:"User Not Found",Data:req.decoded,StatusCode:500,res:res,Status:"Error"});
 })
 
+//Get All Elements
 router.post(Constants.Routes.GetAllSubAdmins.Route, async function (req, res) {
     let Data = await Db.query("SELECT * FROM users WHERE role = 'SubAdmin'",[])
     //Add the record and end the response
